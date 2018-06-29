1ad8f4cffae6044fa0724d095e6a7d4630884a44
Amit Chavan <amitchavan9@gmail.com>
2018-06-29 12:57:05 +0530

    Initial commit.

---
+++ day4/python/function.py
@@ -0,0 +1,8 @@
+#!#/usr/bin/pyhton
+
+def sayhello(msg):
+    print("hi" + msg)
+
+
+if __name__ =="__main__":
+    sayhello("python!")
---
+++ day4/python/lib.py
@@ -0,0 +1,6 @@
+def sayhello(msg):
+    print( "Hello " + msg )
+    print( "Hello " , msg )
+
+def f():
+    print ( ' function f ' )
